1aeac7cfb691c7acacb7d10ebe174248015605ba
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2022-11-30 17:19:54 -0500

    updated screen sizes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
     darkMode: 'class',
     theme: {
         screens: {
-            sm: '480px',
+            sm: '430px',
             md: '768px',
             lg: '976px',
             xl: '1440px',
