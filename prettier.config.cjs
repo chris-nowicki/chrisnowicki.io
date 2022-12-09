7feba6faad9f4c6f9f2fd130946d6f18f26564dc
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2022-12-09 13:58:26 -0500

    updated 'Tech I Work With' to 'TECH STACK'

--- prettier.config.cjs
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    plugins: [require.resolve('prettier-plugin-astro')],
-    plugins: [require('prettier-plugin-tailwindcss')],
-    trailingComma: 'es5',
-    tabWidth: 4,
-    semi: false,
-    singleQuote: true,
-}
