4481fa9d4e8b759d0486fc31b9ff9bb667082d97
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2023-02-04 00:24:24 -0500

    moved types to the types.ts file

--- src/ts/types.ts
+++ src/ts/types.ts
@@ -4,3 +4,45 @@ export type Meta = {
     image: string
     type: string
 }
+
+export interface Settings {
+    navigation: string[]
+    profilePicture: string
+    showProjects: boolean
+}
+
+export interface Projects {
+    projectName: string
+    dateCreated: string
+    tags: string[]
+    gitHubUrl: string
+    liveSiteUrl: string
+}
+
+export interface FeaturedProjects {
+    name: string
+    excerpt: string
+    image: string
+    tags: string[]
+    gitHubUrl: string
+    liveSiteUrl: string
+    slug: string
+}
+
+export interface Technologies {
+    name: string
+    link?: string
+    show: boolean
+}
+
+export interface getLinks {
+    links: string[]
+    showResume: boolean
+    resumeURL: string
+}
+
+export interface Links {
+    title: string
+    reference: string
+    show: boolean
+}
