b0146e37f277e556ee07e1be979bac0c3b0b4115
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2023-02-05 16:17:55 -0500

    implemented astro-icon library

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,34 +1,29 @@
-import { defineConfig } from 'astro/config'
-
-import { astroImageTools } from 'astro-imagetools'
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind'
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import vercel from '@astrojs/vercel/serverless'
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
-import image from '@astrojs/image'
+import image from '@astrojs/image';
 
 // https://astro.build/config
-import sitemap from '@astrojs/sitemap'
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
-import react from '@astrojs/react'
+import react from '@astrojs/react';
+
+// https://astro.build/config
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://chrisnowicki.io',
-    integrations: [
-        tailwind(),
-        image({
-            serviceEntryPoint: '@astrojs/image/sharp',
-        }),
-        sitemap(),
-        react(),
-        astroImageTools,
-    ],
-    output: 'server',
-    adapter: vercel(),
-})
+  site: 'https://chrisnowicki.io',
+  integrations: [tailwind(), image({
+    serviceEntryPoint: '@astrojs/image/sharp'
+  }), sitemap(), react(), prefetch()],
+  output: 'server',
+  adapter: vercel()
+});
