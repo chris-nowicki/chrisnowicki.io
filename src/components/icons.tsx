96e645bc01d53a4c944c36d4263fe6e3d85894b9
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2023-02-10 22:48:34 -0500

    changed theme switcher button to vanilla js from tsx

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -137,9 +137,10 @@ export function TriangleSharp({ size }: icon) {
     )
 }
 
-export function Moon({size = 12}: icon) {
+export function Moon({ size = 12 }: icon) {
     return (
         <svg
+            id="moon"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             width={size}
@@ -150,29 +151,30 @@ export function Moon({size = 12}: icon) {
             className="h-6 w-6 font-bold"
         >
             <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
             ></path>
         </svg>
     )
 }
 
-export function Sun({size = 12}: icon) {
+export function Sun({ size = 12 }: icon) {
     return (
         <svg
+            id="sun"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             width={size}
             height={size}
             viewBox="0 0 24 24"
-            stroke-width="1.5"
+            strokeWidth="1.5"
             stroke="currentColor"
-            className="h-6 w-6 hover:font-bold"
+            className="hidden h-6 w-6 hover:font-bold"
         >
             <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
             ></path>
         </svg>
