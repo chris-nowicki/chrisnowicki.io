77e324d3202bb09970be38f06f6ae38734be7860
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2023-02-16 16:57:57 -0500

    updated button size on technical skills

--- src/components/TechSkills/Button.tsx
+++ src/components/TechSkills/Button.tsx
@@ -4,7 +4,7 @@ export default function Button({ name, data, activeCheck, handleClick }) {
     const isActive = useActive(activeCheck, data)
     return (
         <button
-            className={`text-sm md:text-md rounded border border-neutral-200 p-2 hover:bg-neutral-100 dark:border-gray-900 dark:text-textDark dark:hover:bg-gray-900/25 ${
+            className={`text-sm md:text-lg rounded border border-neutral-200 p-2 hover:bg-neutral-100 dark:border-gray-900 dark:text-textDark dark:hover:bg-gray-900/25 ${
                 isActive ?
                 'dark:border-bg-gray-900 bg-bgDark text-textDark dark:text-[#bd93f9] hover:bg-bgDark hover:text-textDark dark:bg-gray-900  dark:hover:bg-gray-900' :
                 'text-black'
