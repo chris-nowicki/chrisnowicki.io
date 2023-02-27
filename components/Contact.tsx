242360dc98ce2d3b22173c48c026fcde101eac5d
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2023-02-27 13:50:57 -0500

    updated Contact.tsx

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,23 +1,11 @@
-export default function Contact({ showProjects }) {
+export default function Contact() {
     return (
         <div
             id="contact"
-            className={` flex w-full flex-col items-center justify-center rounded ${
-                showProjects
-                    ? ' bg-background-light py-8 text-foreground shadow-lg shadow-background-light/50 dark:bg-background-dark dark:shadow-background-dark/50'
-                    : 'text-black dark:text-foreground'
-            }   px-8 md:px-16 `}
+            className="flex w-full flex-col items-center justify-center rounded bg-background-light py-8 px-8 text-foreground shadow-lg shadow-background-light/50 dark:bg-background-dark dark:shadow-background-dark/50 md:px-16"
         >
             <div className="flex flex-col items-center">
-                <h1
-                    className={`mb-4 text-4xl ${
-                        showProjects
-                            ? ' text-purple-dark'
-                            : 'text-purple-light dark:text-purple-dark'
-                    }`}
-                >
-                    Get In Touch
-                </h1>
+                <h1 className="mb-4 text-4xl text-purple-dark">Get In Touch</h1>
                 <p className="text-center text-lg">
                     I am currently looking for new work. Contact me to say
                     hello, talk about an opportunity, or ask me a question! My
@@ -25,11 +13,7 @@ export default function Contact({ showProjects }) {
                 </p>
                 <a
                     href="mailto:chris@chrisnowicki.io"
-                    className={`mt-8 flex flex-col items-center rounded-lg border-2 px-6 py-2 ${
-                        showProjects
-                            ? 'border-purple-dark  text-purple-dark hover:bg-purple-dark hover:text-foreground'
-                            : 'dark:hover:text-foreground`  border-purple-light text-purple-light hover:bg-purple-light hover:text-foreground dark:border-purple-dark dark:text-purple-dark dark:hover:bg-purple-dark'
-                    }`}
+                    className="mt-8 flex flex-col items-center rounded-lg border-2 border-purple-dark px-6 py-2  text-purple-dark hover:bg-purple-dark hover:text-foreground"
                 >
                     Say Hello
                 </a>
