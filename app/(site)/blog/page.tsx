ba56c3a4b5741c5501c989608a9a81c30f0410a7
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2023-04-28 16:50:20 -0400

    fix: navbar shrinks when dev blog icon clicked

--- app/(site)/blog/page.tsx
+++ app/(site)/blog/page.tsx
@@ -1,4 +1,3 @@
-import Link from 'next/link'
 import format from 'date-fns/format'
 
 // types
@@ -28,13 +27,12 @@ export default async function Blog() {
     <div className="mb-12 px-5 md:px-0">
       <div className="flex flex-col rounded border border-borderColor-light p-4 dark:border-borderColor-dark ">
         <h1 className="flex items-center gap-1 text-2xl md:text-3xl">
-          <Link
+          <a
             href="https://www.dev.to/chrisnowicki/"
-            prefetch={false}
             className="text-black hover:text-purple-light dark:text-white dark:hover:text-purple-dark"
           >
             <DEVTO size={32} />
-          </Link>
+          </a>
           Blog
         </h1>
         <div className="mt-4 flex flex-col text-sm md:flex-row md:gap-3 md:text-base">
@@ -43,10 +41,9 @@ export default async function Blog() {
               <span className="font-bold text-purple-light dark:text-purple-dark">
                 {format(new Date(article.published_at), 'MM.dd.yy')}
               </span>
-              <Link
+              <a
                 key={article.id}
                 href={article.url}
-                prefetch={false}
                 target="_blank"
                 className="flex flex-col"
               >
@@ -55,7 +52,7 @@ export default async function Blog() {
                   {Number(article.page_views_count).toLocaleString()} views /{' '}
                   {article.reading_time_minutes} min read
                 </span>
-              </Link>
+              </a>
             </>
           ))}
         </div>
