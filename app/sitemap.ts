28cbd9d3beb21a32db4b0624d3877686059899a6
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2023-05-23 10:29:33 -0400

    added sitemap.ts

---
+++ app/sitemap.ts
@@ -0,0 +1,26 @@
+import { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://chrisnowicki.io',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://chrisnowicki.io/about',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://chrisnowicki.io/blog',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://chrisnowicki.io/projects',
+      lastModified: new Date(),
+    },
+    {
+      url: 'https://chrisnowicki.io/resume',
+      lastModified: new Date(),
+    },
+  ]
+}
