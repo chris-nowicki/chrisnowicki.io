435e758505bcfe472f0e60606e294ed72442fd09
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2023-09-14 14:43:19 -0400

    Fix: Project section showing active on initial page load if in view

--- hooks/useSectionInView.ts
+++ hooks/useSectionInView.ts
@@ -3,9 +3,10 @@ import { useEffect } from 'react'
 import { useInView } from 'react-intersection-observer'
 import type { SectionName } from '@/types'
 
-export function useSectionInView(sectionName: SectionName, threshold = 0.75) {
+export function useSectionInView(sectionName: SectionName, threshold = 0.75, initialInView = false) {
   const { ref, inView } = useInView({
     threshold,
+    initialInView: initialInView,
   })
   const { setActiveSection, timeOfLastClick } = useActiveSection()
 
