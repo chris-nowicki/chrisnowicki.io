c62cac55349fa4ab8f94810d94f45125ce274d91
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2023-09-14 13:10:01 -0400

    Fix: add blank target for download cv button in Intro section

--- components/Intro/ContactButton.tsx
+++ components/Intro/ContactButton.tsx
@@ -19,6 +19,7 @@ export default function ContactButton({
       href={URL}
       className="group flex items-center gap-2 rounded-full border border-borderColor-light bg-gray-300/20 p-4 px-4 py-2 hover:bg-gray-300/40 dark:border-borderColor-dark dark:bg-gray-300/10  dark:hover:bg-gray-300/20"
       onClick={onClickProps}
+      target={URL === '/#contact' ? '_self' : '_blank'}
     >
       {contactInfo.text}
       {contactInfo.icon}
