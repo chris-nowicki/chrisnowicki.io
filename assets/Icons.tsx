261347968688fd91770ac0a20ec66df4c1f51964
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2023-09-14 08:37:04 -0400

    Fix: added an icon back in that I accidently deleted ... oops.

--- assets/Icons.tsx
+++ assets/Icons.tsx
@@ -69,6 +69,26 @@ export function ArrowIcon({ size }: Icon) {
   )
 }
 
+export function OpenOutline({ size }: Icon) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={size}
+      height={size}
+      viewBox="0 0 512 512"
+    >
+      <path
+        fill="none"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="32"
+        d="M384 224v184a40 40 0 0 1-40 40H104a40 40 0 0 1-40-40V168a40 40 0 0 1 40-40h167.48M336 64h112v112M224 288L440 72"
+      />
+    </svg>
+  )
+}
+
 export function Moon({ size = 12 }: Icon) {
   return (
     <svg
