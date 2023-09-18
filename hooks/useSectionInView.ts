bb2960a1c898efd1faa09bd2ac28168d43fa433e
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2023-09-18 17:33:56 -0400

    Prettier code reformat

--- hooks/useSectionInView.ts
+++ hooks/useSectionInView.ts
@@ -12,11 +12,7 @@ export function useSectionInView(sectionName: SectionName, threshold = 0.75) {
   const { scrollY } = useScroll()
 
   useEffect(() => {
-    if (
-      inView &&
-      Date.now() - timeOfLastClick > 1000 &&
-      scrollY.get() > 0
-    ) {
+    if (inView && Date.now() - timeOfLastClick > 1000 && scrollY.get() > 0) {
       setActiveSection(sectionName)
     }
   }, [inView, setActiveSection, timeOfLastClick, sectionName, scrollY])
