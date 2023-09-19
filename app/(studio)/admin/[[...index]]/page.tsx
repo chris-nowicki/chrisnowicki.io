604dbc4af505264339d9c3821f5fa140ae1fde55
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2023-09-19 13:50:16 -0400

    Fix: Force Sanity Studio to be a dynamic route

--- app/(studio)/admin/[[...index]]/page.tsx
+++ app/(studio)/admin/[[...index]]/page.tsx
@@ -1,7 +1,7 @@
 import { Studio } from './Studio'
 
-// Ensures the Studio route is statically generated
-export const dynamic = 'force-static'
+// Nextjs route segment config
+export const dynamic = 'force-dynamic' // Force dynamic (server) route instead of static page
 
 // Set the right `viewport`, `robots` and `referer` meta tags
 export { metadata } from 'next-sanity/studio/metadata'
