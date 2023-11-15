aebff0e6c63eccea70fd3dba777f831c2365e2f0
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2023-11-15 11:11:03 -0500

    Update featured projects to server component

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -8,29 +8,20 @@ import Contact from '@/components/Contact/Contact'
 import Footer from '@/components/Footer/Footer'
 
 // sanity cms queries
-import {
-  getResume,
-  getFeaturedProjects,
-  getSkills,
-} from '@/sanity/sanity-queries'
+import { getResume, getSkills } from '@/sanity/sanity-queries'
 
 export default async function Home() {
   const resumeData = getResume()
-  const featuredProjectsData = getFeaturedProjects()
   const skillsData = getSkills()
 
-  const [resume, featuredProjects, skills] = await Promise.all([
-    resumeData,
-    featuredProjectsData,
-    skillsData,
-  ])
+  const [resume, skills] = await Promise.all([resumeData, skillsData])
 
   return (
     <div className="flex w-full flex-col items-center px-4 md:px-0">
       <Intro />
       <TechStack />
       <SectionDivider />
-      <FeaturedProjects projects={featuredProjects} />
+      <FeaturedProjects />
       <SectionDivider />
       <Skills data={skills} />
       <SectionDivider />
--- components/FeaturedProjects/FeaturedProjects.tsx
+++ components/FeaturedProjects/FeaturedProjects.tsx
@@ -1,21 +1,12 @@
-'use client'
-import { useSectionInView } from '@/hooks/useSectionInView'
 import ContentCarousel from './Carousel/ContentCarousel'
 import SectionHeading from '@/components/SectionHeading'
-import type { ProjectType } from '@/types/types'
+import { getFeaturedProjects } from '@/sanity/sanity-queries'
 
-export default function FeaturedProjects({
-  projects,
-}: {
-  projects: ProjectType
-}) {
-  const { ref } = useSectionInView('Projects', 0.5)
+export default async function FeaturedProjects() {
+  const projects = await getFeaturedProjects()
+
   return (
-    <section
-      ref={ref}
-      id="projects"
-      className="flex scroll-mt-20 flex-col md:scroll-mt-16"
-    >
+    <section className="flex scroll-mt-20 flex-col md:scroll-mt-16">
       <SectionHeading>Projects</SectionHeading>
       <div className="flex w-full max-w-3xl flex-col items-center lg:items-start">
         <div className="flex w-full flex-col items-center gap-4  md:flex-row md:flex-wrap md:items-start ">
