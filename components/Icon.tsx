da024f1dd3d11fdbaf37bf97f3f3251968eca4a2
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-01-15 17:28:39 -0500

    Fix: devto icon dark mode not styled correctly

--- components/Icon.tsx
+++ components/Icon.tsx
@@ -1,3 +1,6 @@
+'use client'
+import { useTheme } from 'next-themes'
+
 type IconProps = {
   id: string
   size: number
@@ -5,9 +8,14 @@ type IconProps = {
 }
 
 export default function Icon({ id, size, className }: IconProps): JSX.Element {
+  const { theme } = useTheme()
+  const isDarkMode = theme === 'dark'
+
+  const iconName = id === 'logo-devto' && isDarkMode ? 'logo-devto-dark' : id
+
   return (
     <svg width={size} height={size} className={className}>
-      <use href={`/icons/sprite.svg#${id}`} />
+      <use href={`/icons/sprite.svg#${iconName}`} />
     </svg>
   )
 }
