f2c455787772e169592e1d9e1c2fad0a6db94e74
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-03-04 11:32:26 -0500

    refactor: remove date from speaking video links

--- components/Engagements/Speaking.tsx
+++ components/Engagements/Speaking.tsx
@@ -13,7 +13,6 @@ export default function Speaking() {
       <Card className="w-full transition-all duration-100 ease-in-out hover:border-primary md:w-[375px]">
         <CardHeader>
           <CardTitle>How to Improve Your Home WiFi Speeds ⚡️</CardTitle>
-          <CardDescription>February 9th, 2024</CardDescription>
         </CardHeader>
         <CardContent>
           <YouTubeEmbed embedId="ZSMU5Avf-T0?si=8XR7aRmDw_0hSChK" />
@@ -23,7 +22,6 @@ export default function Speaking() {
       <Card className="w-full transition-all duration-100 ease-in-out hover:border-primary md:w-[375px]">
         <CardHeader>
           <CardTitle>The Complexity of ADHD Treatment 🧠</CardTitle>
-          <CardDescription>August 30th, 2023</CardDescription>
         </CardHeader>
         <CardContent>
           <YouTubeEmbed embedId="P4aMxBWwLD0?si=AJbMu8fqJ-U9EQNN" />
