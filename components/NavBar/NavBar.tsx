7b7482cd7ea60511633dc7329af30b298fac521a
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-03-04 22:11:47 -0500

    fix: mobile navbar dark mode was not working correctly

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -38,7 +38,7 @@ export default function NavBar() {
   return (
     <nav className="mb-24 flex w-full max-w-3xl justify-center md:mb-12">
       {/* nav bar */}
-      <div className="fixed z-30 flex w-full items-center justify-between bg-white py-4 shadow md:relative md:bg-transparent md:px-0 md:py-0 md:shadow-none">
+      <div className="fixed z-30 flex w-full items-center justify-between bg-background py-4 shadow dark:shadow-foreground/20 md:relative md:px-0 md:py-0 md:shadow-none">
         {/* nav bar menu */}
         <div
           className={clsx(
