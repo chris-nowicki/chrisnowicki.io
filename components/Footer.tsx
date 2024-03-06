858ce0a0e416e943a600f2f960963d7b51a4c2d2
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-03-06 13:20:50 -0500

    refactor: updates to new footer to make styling unique and remove metrics in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
-import { Separator } from './ui/separator'
 import SocialMetrics from './SocialMetrics/SocialMetrics'
+import SectionDivider from './SectionDivider'
 
 const footerLinks = [
   { name: '/home', href: '/' },
@@ -13,18 +13,18 @@ const footerLinks = [
 
 export default async function Footer() {
   return (
-    <footer className="my-10 flex flex-col items-center px-4 text-left md:px-0 md:text-center">
-      <Separator className="my-5 md:my-10" />
-      <div className="flex w-full flex-col justify-between gap-4 md:flex-wrap">
-        <div className="flex w-full flex-col items-center md:w-1/2 md:items-start">
-          <span className="text-3xl font-semibold mb-4 md:mb-0">Sitemap</span>
+    <footer className="mb-20 flex flex-col items-center px-4 text-left md:px-0 md:text-center">
+      <SectionDivider />
+      <div className="flex w-full flex-col justify-between gap-4 rounded-xl border p-4 shadow-xl shadow-primary/20 md:flex-row">
+        <div className="flex w-full flex-col items-center md:w-1/2 md:items-start md:justify-between">
+          <span className="mb-4 text-3xl font-semibold">Sitemap</span>
           {footerLinks.map((link, index) => (
             <Link key={index} href={link.href} className="hover:text-primary">
               {link.name}
             </Link>
           ))}
         </div>
-        <div className="flex w-full flex-col items-center justify-between gap-4 md:w-1/2 md:items-start md:gap-0">
+        <div className="flex w-full flex-col items-center justify-between gap-4 md:w-1/2  md:gap-0">
           <span className="text-3xl font-semibold">Links</span>
           <SocialMetrics metrics={false} footer={true} />
         </div>
