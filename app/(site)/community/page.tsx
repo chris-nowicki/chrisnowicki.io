0ad31fb42324955a09c02a07b69ccaf7de54ae8c
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-03-13 15:47:01 -0400

    feat: update and add dynamic OG images

--- app/(site)/community/page.tsx
+++ app/(site)/community/page.tsx
@@ -10,8 +10,41 @@ import TechnicalWriting from './TechnicalWriting'
 import OSS from '@/components/Engagements/OSS'
 import Conferences from '@/components/Engagements/Conferences'
 import Speaking from '@/components/Engagements/Speaking'
+import { Metadata } from 'next'
 
 export const revalidate = 60
+
+// metadata
+const title: string = `Chris Nowicki's Community Involvement`
+const description: string = `OSS, Technical Writing, Conferences, and Speaking Contributions`
+
+const ogSearchParams = new URLSearchParams()
+ogSearchParams.set('page', 'COMMUNITY')
+ogSearchParams.set('description', description)
+
+export const metadata: Metadata = {
+  title: title,
+  description: description,
+  openGraph: {
+    title: title,
+    description: description,
+    url: 'https://chrisnowicki.io/community',
+    images: [
+      {
+        url: `/api/og?${ogSearchParams.toString()}`,
+        width: 1200,
+        height: 630,
+        alt: title,
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: title,
+    description: description,
+    images: [`/api/og?${ogSearchParams.toString()}`],
+  },
+}
 
 export default async function Blog() {
   return (
