f8d742ec6b4ccdfdf3e6f073776c75e25b41499c
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-03-19 11:52:53 -0400

    fix: add back in skills to reference in projects

---
+++ sanity/schemas/skills.ts
@@ -0,0 +1,36 @@
+import { CodeBlockIcon } from '@sanity/icons'
+
+export default {
+  name: 'tech',
+  type: 'document',
+  title: 'Skills',
+  icon: CodeBlockIcon,
+  __experimental_formPreviewTitle: false,
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string',
+    },
+    {
+      name: 'category',
+      title: 'Category',
+      type: 'string',
+      options: {
+        list: [
+          { title: 'Languages', value: 'lang' },
+          { title: 'Frameworks/Libraries', value: 'framelib' },
+          { title: 'Databases', value: 'db' },
+          { title: 'Cloud/Serverless', value: 'cloud' },
+          { title: 'Operating Systems', value: 'os' },
+          { title: 'Tools', value: 'tools' },
+        ],
+      },
+    },
+    {
+      name: 'link',
+      title: 'Link',
+      type: 'string',
+    },
+  ],
+}
