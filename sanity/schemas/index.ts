adeba8a462c0c99683bee99b5d38939cc76db977
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-03-28 10:28:03 -0400

    fix: skills schema missing from sanity

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -3,5 +3,13 @@ import intro from './pages/intro'
 import featuredProjects from './pages/featuredProjects'
 import resume from './pages/resume'
 import projects from './projects'
+import skills from './skills'
 
-export const schemaTypes = [seo, intro, featuredProjects, resume, projects]
+export const schemaTypes = [
+  seo,
+  intro,
+  featuredProjects,
+  resume,
+  projects,
+  skills,
+]
