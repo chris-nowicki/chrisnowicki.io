9b521aed8aa8990c35be95b9dac136761cb751d9
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-04-08 15:52:25 -0400

    build: removed unused functions
    '

--- utils/utils.ts
+++ utils/utils.ts
@@ -2,36 +2,6 @@ import { type ClassValue, clsx } from 'clsx'
 import { twMerge } from 'tailwind-merge'
 import { Post } from '#site/content'
 
-// EMAIL (CONTACT FORM) VALIDATIONS
-// validate if the contact form email and message is a string
-export const validateString = (
-  value: unknown,
-  maxLength: number
-): value is string => {
-  if (!value || typeof value !== 'string' || value.length > maxLength) {
-    return false
-  }
-
-  return true
-}
-
-// get the error message if the email has issues sending through resend
-export const getErrorMessage = (error: unknown): string => {
-  let message: string
-
-  if (error instanceof Error) {
-    message = error.message
-  } else if (error && typeof error === 'object' && 'message' in error) {
-    message = String(error.message)
-  } else if (typeof error === 'string') {
-    message = error
-  } else {
-    message = 'Something went wrong'
-  }
-
-  return message
-}
-
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
