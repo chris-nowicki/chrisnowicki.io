cb18f53bb171a81d7ca8bf1991ad26ac8f297cbf
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-04-16 16:39:25 -0400

    updated dependencies & removed sanity studio theme

--- sanity.config.tsx
+++ sanity.config.tsx
@@ -5,16 +5,11 @@ import { visionTool } from '@sanity/vision'
 import { media } from 'sanity-plugin-media'
 import { schemaTypes } from '@/sanity/schemas'
 
-// custom studio theme
-import { theme as _theme } from '@/sanity/theme'
-const theme = _theme as import('sanity').StudioTheme
-
 const config = defineConfig({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion: '2021-03-25',
   basePath: '/admin',
-  theme,
   title: 'chrisnowicki.io',
 
   plugins: [
