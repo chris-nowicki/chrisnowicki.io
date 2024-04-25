b840572498da83b1cd8400b146acd8aabfe3f837
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-04-25 10:58:05 -0500

    chore: moved sw hw list to lib/utils.ts

--- app/(site)/uses/page.tsx
+++ app/(site)/uses/page.tsx
@@ -2,6 +2,7 @@ import SectionHeading from '@/components/SectionHeading'
 import AffiliateLink from '@/components/AffiliateLink'
 import { Metadata } from 'next'
 import CldImage from '@/components/CldImage'
+import { hw, sw } from '@/lib/uses'
 
 // metadata
 const title: string = `Chris Nowicki's Uses`
@@ -17,7 +18,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: title,
     description: description,
-    url: 'https://chrisnowicki.io/blog',
+    url: 'https://chrisnowicki.io/uses',
     images: [
       {
         url: `/api/og?${ogSearchParams.toString()}`,
@@ -34,41 +35,6 @@ export const metadata: Metadata = {
     images: [`/api/og?${ogSearchParams.toString()}`],
   },
 }
-
-const hw = [
-  { name: 'LG 27" 5K UHD Monitor', link: 'https://a.co/d/3GTjqUC' },
-  { name: 'LG 28" DualUp Monitor', link: 'https://a.co/d/dIDu3P9' },
-  { name: 'CalDigit TS4 Docking Station', link: 'https://a.co/d/9vBnxSh' },
-  { name: 'Lacie 2BIG Raid', link: 'https://a.co/d/8t33fbe' },
-  { name: 'MX Master 3S Mouse', link: 'https://a.co/d/fLPmSLo' },
-  { name: 'Apple Magic Trackpad', link: 'https://a.co/d/3Py1Fc8' },
-  { name: 'Keychron Q1 Pro Keyboard', link: 'https://a.co/d/405cpbJ' },
-  {
-    name: 'OMOTON Vertical Laptop Desktop Stand',
-    link: 'https://a.co/d/3HkFNW7',
-  },
-  {
-    name: 'Audio Engine A2+ Speakers',
-    link: 'https://a.co/d/6IRbVvj',
-  },
-]
-
-const sw = [
-  { name: '1Password', link: 'https://1password.com/' },
-  { name: 'Arc Browser', link: 'https://arc.net/' },
-  { name: 'CleanMyMac', link: 'https://macpaw.com/cleanmymac' },
-  { name: 'CleanShot X', link: 'https://cleanshot.com/' },
-  { name: 'Discord', link: 'https://discord.com/' },
-  { name: 'Figma', link: 'https://www.figma.com/' },
-  { name: 'MindNode', link: 'https://mindnode.com/' },
-  { name: 'RayCast', link: 'https://www.raycast.com/' },
-  { name: 'Rectangle', link: 'https://rectangleapp.com/' },
-  { name: 'Slack', link: 'https://slack.com/' },
-  { name: 'Spotify', link: 'https://www.spotify.com/' },
-  { name: 'Things3', link: 'https://culturedcode.com/things/' },
-  { name: 'VS Code', link: 'https://code.visualstudio.com/' },
-  { name: 'Zoom', link: 'https://zoom.us/' },
-]
 
 export default function Uses() {
   return (
---
+++ lib/uses.ts
@@ -0,0 +1,34 @@
+export const hw = [
+  { name: 'LG 27" 5K UHD Monitor', link: 'https://a.co/d/3GTjqUC' },
+  { name: 'LG 28" DualUp Monitor', link: 'https://a.co/d/dIDu3P9' },
+  { name: 'CalDigit TS4 Docking Station', link: 'https://a.co/d/9vBnxSh' },
+  { name: 'Lacie 2BIG Raid', link: 'https://a.co/d/8t33fbe' },
+  { name: 'MX Master 3S Mouse', link: 'https://a.co/d/fLPmSLo' },
+  { name: 'Apple Magic Trackpad', link: 'https://a.co/d/3Py1Fc8' },
+  { name: 'Keychron Q1 Pro Keyboard', link: 'https://a.co/d/405cpbJ' },
+  {
+    name: 'OMOTON Vertical Laptop Desktop Stand',
+    link: 'https://a.co/d/3HkFNW7',
+  },
+  {
+    name: 'Audio Engine A2+ Speakers',
+    link: 'https://a.co/d/6IRbVvj',
+  },
+]
+
+export const sw = [
+  { name: '1Password', link: 'https://1password.com/' },
+  { name: 'Arc Browser', link: 'https://arc.net/' },
+  { name: 'CleanMyMac', link: 'https://macpaw.com/cleanmymac' },
+  { name: 'CleanShot X', link: 'https://cleanshot.com/' },
+  { name: 'Discord', link: 'https://discord.com/' },
+  { name: 'Figma', link: 'https://www.figma.com/' },
+  { name: 'MindNode', link: 'https://mindnode.com/' },
+  { name: 'RayCast', link: 'https://www.raycast.com/' },
+  { name: 'Rectangle', link: 'https://rectangleapp.com/' },
+  { name: 'Slack', link: 'https://slack.com/' },
+  { name: 'Spotify', link: 'https://www.spotify.com/' },
+  { name: 'Things3', link: 'https://culturedcode.com/things/' },
+  { name: 'VS Code', link: 'https://code.visualstudio.com/' },
+  { name: 'Zoom', link: 'https://zoom.us/' },
+]
