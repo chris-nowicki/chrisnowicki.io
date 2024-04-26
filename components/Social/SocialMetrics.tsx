b9065b945da86bf31aad9be9317a12895d41750d
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-04-25 21:57:23 -0500

    moved resume to store locally

--- components/Social/SocialMetrics.tsx
+++ components/Social/SocialMetrics.tsx
@@ -1,6 +1,5 @@
 import { socialLinks } from '@/lib/data'
 import { cn } from '@/utils/utils'
-import { getResume } from '@/lib/appwrite-client'
 import Link from 'next/link'
 import Icon from '../Icon'
 import { buttonVariants } from '../ui/button'
@@ -17,8 +16,6 @@ export default async function SocialMetrics({
   metrics = true,
   footer = false,
 }: SocialMetricsProps) {
-  const resumeURL = await getResume()
-
   return (
     <section
       className={cn(
@@ -35,7 +32,7 @@ export default async function SocialMetrics({
       >
         {/* resume */}
         <Link
-          href={resumeURL}
+          href="/ChrisNowicki_Resume.pdf"
           className={cn(
             buttonVariants({ variant: 'outline' }),
             'group flex items-center justify-center gap-2 rounded-lg py-6 hover:border-primary hover:bg-transparent hover:shadow group-hover:text-primary',
