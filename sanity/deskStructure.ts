43e9fa73a8662578e812600626bb29c87257dd28
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-04-25 20:35:20 -0500

    refactor: remove sanity cms desk structure for seo and intro

--- sanity/deskStructure.ts
+++ sanity/deskStructure.ts
@@ -5,19 +5,6 @@ export const myStructure = (S: any) =>
     .title('Content')
     .items([
       S.listItem()
-        .title('Site Settings')
-        .icon(CogIcon)
-        .child(
-          S.list()
-            .title('Site Settings')
-            .items([
-              S.listItem()
-                .title('SEO')
-                .child(S.document().schemaType('seo').documentId('seo')),
-            ])
-        ),
-      S.divider(),
-      S.listItem()
         .title('Portfolio')
         .child(
           S.list()
@@ -26,9 +13,6 @@ export const myStructure = (S: any) =>
             // Add items to the array
             // Each will pull one of our new singletons
             .items([
-              S.listItem()
-                .title('Intro')
-                .child(S.document().schemaType('intro').documentId('intro')),
               S.listItem()
                 .title('Featured Projects')
                 .child(
@@ -41,16 +25,10 @@ export const myStructure = (S: any) =>
                 .child(S.document().schemaType('resume').documentId('resume')),
             ])
         ),
-      S.divider(),
       ...S.documentTypeListItems().filter(
         (listItem: any) =>
-          ![
-            'seo',
-            'home',
-            'intro',
-            'featuredProjects',
-            'resume',
-            'media.tag',
-          ].includes(listItem.getId())
+          !['featuredProjects', 'resume', 'media.tag'].includes(
+            listItem.getId()
+          )
       ),
     ])
--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,15 +1,6 @@
-import seo from './pages/seo'
-import intro from './pages/intro'
 import featuredProjects from './pages/featuredProjects'
 import resume from './pages/resume'
 import projects from './projects'
 import skills from './skills'
 
-export const schemaTypes = [
-  seo,
-  intro,
-  featuredProjects,
-  resume,
-  projects,
-  skills,
-]
+export const schemaTypes = [featuredProjects, resume, projects, skills]
