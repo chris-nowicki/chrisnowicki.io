def772c095b2998f145c6089010b49857134f1b2
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-05-02 08:38:41 -0400

    refactor: renamed variables

--- components/Hero/Intro.tsx
+++ components/Hero/Intro.tsx
@@ -3,20 +3,20 @@ import CldImage from '../CldImage'
 import { getMarkdownData } from '@/utils/hero'
 import '@/styles/intro.css'
 
-type HeroData = {
+type Intro = {
   image: string
   markdown: string
 }
 
 export default async function Intro() {
-  const heroData: HeroData = await getMarkdownData('intro.md')
+  const intro: Intro = await getMarkdownData('intro.md')
 
   return (
     <section className="mx-4 flex flex-wrap-reverse justify-center md:mx-0 md:flex-nowrap md:justify-start">
       <div className="mr-0 flex w-full flex-col items-center text-xl md:mr-6 md:items-start">
         {/* intro text */}
         <div
-          dangerouslySetInnerHTML={{ __html: heroData.markdown }}
+          dangerouslySetInnerHTML={{ __html: intro.markdown }}
           className="intro"
         />
 
@@ -42,7 +42,7 @@ export default async function Intro() {
           className="rounded-xl bg-accent p-2 shadow-lg "
           width={255}
           height={281}
-          src={heroData.image}
+          src={intro.image}
           alt="chris nowicki"
         />
       </div>
