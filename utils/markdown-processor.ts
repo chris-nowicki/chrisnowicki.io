fce3331160638f8d852d3b8a70f5dba5c425c04b
Chris Nowicki <102450568+chris-nowicki@users.noreply.github.com>
2024-09-03 15:53:34 -0400

    update markdown processor with caching

--- utils/markdown-processor.ts
+++ utils/markdown-processor.ts
@@ -10,30 +10,43 @@ import rehypeAutolinkHeadings from 'rehype-autolink-headings'
 import rehypeRaw from 'rehype-raw'
 import rehypeStringify from 'rehype-stringify'
 
+const processor = unified()
+  .use(remarkParse)
+  .use(remarkGfm)
+  .use(remarkUnwrapImages)
+  .use(remarkRehype, { allowDangerousHtml: true })
+  .use(rehypeSlug)
+  .use(rehypePrettyCode, { theme: 'dracula' })
+  .use(rehypeExternalLinks, {
+    target: '_blank',
+    rel: ['noopener', 'noreferrer'],
+  })
+  .use(rehypeAutolinkHeadings, {
+    behavior: 'wrap',
+    properties: {
+      className: ['subheading-anchor'],
+      ariaLabel: 'Link to section',
+    },
+  })
+  .use(rehypeRaw)
+  .use(rehypeStringify)
+
+const cache = new Map<string, string>()
+
 export const transformMarkdown = async (markdown: string) => {
-  const processor = unified()
-    .use(remarkParse)
-    .use(remarkGfm)
-    .use(remarkUnwrapImages)
-    .use(remarkRehype, { allowDangerousHtml: true })
-    .use(rehypeSlug)
-    .use(rehypePrettyCode, { theme: 'dracula' })
-    .use(rehypeExternalLinks, {
-      target: '_blank',
-      rel: ['noopener', 'noreferrer'],
-    })
-    .use(rehypeAutolinkHeadings, {
-      behavior: 'wrap',
-      properties: {
-        className: ['subheading-anchor'],
-        ariaLabel: 'Link to section',
-      },
-    })
-    .use(rehypeRaw)
-    .use(rehypeStringify)
+  if (cache.has(markdown)) {
+    return cache.get(markdown)
+  }
 
-  const file = await processor.process(markdown)
-  const html = file.toString()
+  try {
+    const file = await processor.process(markdown)
+    const html = file.toString()
 
-  return html
+    cache.set(markdown, html)
+
+    return html
+  } catch (error) {
+    console.error('Error processing markdown:', error)
+    throw error
+  }
 }
